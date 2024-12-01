3c42517040a93136d5152cc30f12651a652f0d21
Giovannibriglia <giovanni.briglia@unimore.it>
2024-12-01 10:36:01 +0100

    handle intervention in batch

--- vni/non_parametric_estimators/kde.py
+++ vni/non_parametric_estimators/kde.py
@@ -30,12 +30,14 @@ class KernelDensityEstimator(BaseNonParametricEstimator):
 
         batch_size = X_query.shape[0]
 
-        Y_prior = self.XY_prior[self.Y_indices, :].T  # [n_samples_data, n_features_Y]
+        Y_prior = self.XY_prior[
+            self.Y_indices, :
+        ].T  # Shape: [n_samples_data, n_features_Y]
 
         # Define or sample Y values based on the query.
         Y_values = self._define_Y_values(
             Y_query, n_samples, batch_size
-        )  # Output: [batch_size, n_target_features, n_samples] if Y_query is None, else [batch_size, n_target_features, 1]
+        )  # Shape: [batch_size, n_target_features, n_samples] if Y_query is None, else [batch_size, n_target_features, 1]
 
         pdf = torch.zeros_like(Y_values, device=Y_values.device)
 
@@ -44,18 +46,18 @@ class KernelDensityEstimator(BaseNonParametricEstimator):
 
                 y_query_feature = Y_values[:, feature_idx, value].unsqueeze(
                     -1
-                )  # [batch_size, 1]
+                )  # Shape: [batch_size, 1]
                 y_samples_feature = Y_prior[:, feature_idx].unsqueeze(
                     -1
-                )  # [n_samples_data, 1]
+                )  # Shape: [n_samples_data, 1]
 
                 y_kernel = self._compute_density(
                     y_samples_feature, y_query_feature
-                )  # [batch_size, n_samples_data]
+                )  # Shape: [batch_size, n_samples_data]
 
                 X_prior = self.XY_prior[
                     self.X_indices, :
-                ].T  # [n_samples_data, n_features_X]
+                ].T  # Shape: [n_samples_data, n_features_X]
 
                 if self.intervention_indices is None:
                     if X_do is None:
@@ -68,39 +70,39 @@ class KernelDensityEstimator(BaseNonParametricEstimator):
                     if X_do is None:
                         X_do_prior = self.XY_prior_intervention[
                             self.intervention_indices, :
-                        ].T  # [n_samples_data, n_features_X_do]
+                        ].T  # Shape: [n_samples_data, n_features_X_do]
                     else:
                         tolerance = torch.full_like(
                             X_do, 1e-8, device=X_do.device
-                        )  # [batch_size, n_features_X_do]
+                        )  # Shape: [batch_size, n_features_X_do]
                         normal_dist = Normal(X_do, tolerance)
 
                         X_do_prior = normal_dist.sample(
                             (self.XY_prior.shape[1],)
                         ).permute(
                             1, 0, 2
-                        )  # [batch_size, n_samples_data, n_features_X_do]
+                        )  # Shape: [batch_size, n_samples_data, n_features_X_do]
 
                 if X_do_prior is None:
                     x_kernel = self._compute_density(
                         X_prior, X_query
-                    )  # [batch_size, n_samples_data]
+                    )  # Shape: [batch_size, n_samples_data]
 
                     marginal_density = x_kernel.sum(dim=-1) / X_prior.size(
                         0
-                    )  # [batch_size]
+                    )  # Shape: [batch_size]
 
                     # Compute joint KDE
                     joint_density = (x_kernel * y_kernel).sum(dim=-1) / Y_values[
                         :, feature_idx, :
                     ].size(
                         0
-                    )  # [batch_size]
+                    )  # Shape: [batch_size]
 
                     # Compute conditional P(X | Y = y)
                     cpd_feature = joint_density / (
                         marginal_density + 1e-8
-                    )  # Avoid division by zero [batch_size]
+                    )  # Avoid division by zero Shape: [batch_size]
 
                     pdf[:, feature_idx, value] = cpd_feature
 
@@ -125,63 +127,65 @@ class KernelDensityEstimator(BaseNonParametricEstimator):
                     else:
                         X_samples = torch.cat(
                             (X_query, X_do), dim=1
-                        )  # [batch_size, n_features_X+n_features_X_do]
+                        )  # Shape: [batch_size, n_features_X+n_features_X_do]
 
                     if (
                         X_do_prior.dim() == 3
-                    ):  # [batch_size, n_samples_data, n_features_X_do]
+                    ):  # Shape: [batch_size, n_samples_data, n_features_X_do]
                         for batch_index in range(Y_values.shape[0]):
 
                             X_data = torch.cat(
                                 (X_prior, X_do_prior[batch_index, :]), dim=1
-                            )  # [n_samples_data, n_features_X+n_features_X_dp]
+                            )  # Shape: [n_samples_data, n_features_X+n_features_X_dp]
 
                             x_kernel = self._compute_density(
                                 X_data, X_samples
-                            )  # [batch_size, n_samples_data]
+                            )  # Shape: [batch_size, n_samples_data]
 
                             marginal_density = x_kernel.sum(dim=-1) / X_data.size(
                                 0
-                            )  # [batch_size]
+                            )  # Shape: [batch_size]
 
                             # Compute joint KDE
                             joint_density = (x_kernel * y_kernel).sum(
                                 dim=-1
                             ) / Y_values[batch_index, feature_idx, :].size(
                                 0
-                            )  # [batch_size]
+                            )  # Shape: [batch_size]
 
                             # Compute conditional P(X | Y = y)
                             cpd_feature = torch.mean(
                                 joint_density / (marginal_density + 1e-8)
-                            )  # Avoid division by zero [1]
+                            )  # Avoid division by zero. Shape: [1]
 
                             pdf[batch_index, feature_idx, value] = cpd_feature
 
-                    elif X_do_prior.dim() == 2:  # [n_samples_data, n_features_X_do]
+                    elif (
+                        X_do_prior.dim() == 2
+                    ):  # Shape: [n_samples_data, n_features_X_do]
                         X_data = torch.cat(
                             (X_prior, X_do_prior), dim=1
-                        )  # [n_samples_data, n_features_X+n_features_X_dp]
+                        )  # Shape: [n_samples_data, n_features_X+n_features_X_dp]
 
                         x_kernel = self._compute_density(
                             X_data, X_samples
-                        )  # [batch_size, n_samples_data]
+                        )  # Shape: [batch_size, n_samples_data]
 
                         marginal_density = x_kernel.sum(dim=-1) / X_data.size(
                             0
-                        )  # [batch_size]
+                        )  # Shape: [batch_size]
 
                         # Compute joint KDE
                         joint_density = (x_kernel * y_kernel).sum(dim=-1) / Y_values[
                             :, feature_idx, :
                         ].size(
                             0
-                        )  # [batch_size]
+                        )  # Shape: [batch_size]
 
                         # Compute conditional P(X | Y = y)
                         cpd_feature = joint_density / (
                             marginal_density + 1e-8
-                        )  # Avoid division by zero [batch_size]
+                        )  # Avoid division by zero. Shape: [batch_size]
 
                         pdf[:, feature_idx, value] = cpd_feature
 
@@ -219,22 +223,6 @@ class KernelDensityEstimator(BaseNonParametricEstimator):
         """
 
         return x[:, None, :] - y[None, :, :]  # Shape: [n_queries, n_samples, d]
-
-    def _apply_kernel(self, x, y):
-        """
-        General function to apply a kernel function between two tensors.
-
-        Args:
-            x: torch.Tensor of shape [n_queries, d], query points.
-            y: torch.Tensor of shape [n_samples, d], data points.
-            kernel_function: Callable, kernel function to compute values.
-            **kwargs: Additional arguments to pass to the kernel function.
-
-        Returns:
-            kernel_values: torch.Tensor of shape [n_queries, n_samples], kernel values.
-        """
-        diff = self._compute_diff(x, y)
-        return self._kernel_function(diff)
 
     @abstractmethod
     def _kernel_function(self, dist_sq):
